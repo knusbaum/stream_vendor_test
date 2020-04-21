8c7fa2919efe8f5681237ddbd6c342fbb9a6cf0f
Kyle Nusbaum <kyle@datadog.com>
2020-04-21 12:53:36 -0500

    No vendoring. Build is OK

---
+++ main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/health"
+	healthpb "google.golang.org/grpc/health/grpc_health_v1"
+	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
+	grpctrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/google.golang.org/grpc"
+)
+
+var (
+	port  = flag.Int("port", 50051, "the port to serve on")
+	sleep = flag.Duration("sleep", time.Second*5, "duration between changes in health")
+
+	system = "" // empty string represents the health of the system
+)
+
+type echoServer struct {
+	pb.UnimplementedEchoServer
+}
+
+func (e *echoServer) UnaryEcho(ctx context.Context, req *pb.EchoRequest) (*pb.EchoResponse, error) {
+	return &pb.EchoResponse{
+		Message: fmt.Sprintf("hello from localhost:%d", *port),
+	}, nil
+}
+
+var _ pb.EchoServer = &echoServer{}
+
+func main() {
+	tracer.Start()
+	defer tracer.Stop()
+	
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	ui := grpctrace.UnaryServerInterceptor(grpctrace.WithServiceName("my-grpc-server"))
+	s := grpc.NewServer(grpc.UnaryInterceptor(ui))
+	healthcheck := health.NewServer()
+	healthpb.RegisterHealthServer(s, healthcheck)
+	pb.RegisterEchoServer(s, &echoServer{})
+
+	go func() {
+		// asynchronously inspect dependencies and toggle serving status as needed
+		next := healthpb.HealthCheckResponse_SERVING
+
+		for {
+			healthcheck.SetServingStatus(system, next)
+
+			if next == healthpb.HealthCheckResponse_SERVING {
+				next = healthpb.HealthCheckResponse_NOT_SERVING
+			} else {
+				next = healthpb.HealthCheckResponse_SERVING
+			}
+
+			time.Sleep(*sleep)
+		}
+	}()
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
